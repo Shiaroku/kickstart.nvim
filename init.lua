110150767d02ba390251c4628e8ebd04ddb76670
Sébastien MEYER <sbmeyer68@gmail.com>
2024-03-08 15:34:42 +0100

    Initial lsp servers config

--- init.lua
+++ init.lua
@@ -534,7 +534,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        julials = {},
+        pyright = {},
+        texlab = {},
+        r_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
